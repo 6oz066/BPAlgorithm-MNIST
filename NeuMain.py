eea7dbcc53f8e368fe03e716730492181ac9a2a7
szcwq <akayue05@gmail.com>
2024-11-19 14:46:36 +0800

    TBD without predict function

--- NeuMain.py
+++ NeuMain.py
@@ -16,15 +16,27 @@ def sigmoid(x):
 
 # Derivative of sigmoid, use the parameter from f(x)
 def d_sigmoid(f):
-    # Jump out of vanishing gradient
-    if f==1.0:
-        return 0.001*random()*f
-    else:
         return f*(1.0-f)
 
-def d_ad_sigmoid(f):
-    return f*(1-f)
+def tanh(x):
+    return np.tanh(x)
 
+def d_tanh(f):
+    return 1-f**2
+
+
+# Jump out of Vanishing Gradient
+def jp_vg(martix):
+    for i in martix:
+        for j in i:
+            if j==0:
+                j+=random()
+            elif j==1:
+                j-=random()
+
+# Judgement Function for output result
+def judge_result(output,test):
+    return (abs(output-test)<0.1)
 
 """
 Class Function Design
@@ -53,7 +65,7 @@ class Hidden_layer:
         self.learning = learning
 
         # Build the structure
-        self.weight_martix = np.random.rand(self.num_col,self.num_line)
+        self.weight_martix = 0.0375* np.random.rand(self.num_col,self.num_line)
         self.threshold_martix = np.random.rand(self.num_col,self.num_line)
         self.bias = np.zeros((self.num_col,self.num_line))
         self.loss = np.zeros((self.num_col,self.num_line))
@@ -71,10 +83,10 @@ class Hidden_layer:
             for j in range(self.num_line):
                 if(i==0):
                     index = list([i, j])
-                    self.Output_value[i,j] = self.output_function(np.sum(np.dot(Input_martix, self.weight_martix[i, :])),sigmoid,index)
+                    self.Output_value[i,j] = self.output_function(np.sum(np.dot(Input_martix, self.weight_martix[i])),tanh,index)
                 else:
                     index = list([i, j])
-                    self.Output_value[i,j] = self.output_function(np.sum(np.dot(self.Output_value[i-1,:], self.weight_martix[i, :])),sigmoid,index)
+                    self.Output_value[i,j] = self.output_function(np.sum(np.dot(self.Output_value[i-1], self.weight_martix[i])),tanh,index)
 
         return self.Output_value[-1]
 
@@ -86,25 +98,31 @@ class Hidden_layer:
 
         # Use the loss from output layer to calculate
         for i in reversed(range(self.num_col)):
+            # Avoid Vanishing Gradient
+            jp_vg(self.weight_martix)
+            jp_vg(self.threshold_martix)
+
             if i==self.num_col-1:
                 hidden_bias = Output_layer.backward_learning(y_train)
                 for j in range(self.num_col):
-                    self.bias[i,j] = hidden_bias*self.weight_martix[i,j]*d_sigmoid(self.Output_value[i,j])
+                    self.bias[i,j] = hidden_bias*self.weight_martix[i,j]*d_tanh(self.Output_value[i,j])
                     self.weight_martix[i,j] -= self.bias[i,j] * self.learning
                     self.threshold_martix[i,j] -= self.bias[i,j] *self.learning
             else:
-                self.bias[i,:] = self.bias_function(i,d_ad_sigmoid)
-                self.weight_martix[i,:] -= self.bias[i,:] * self.learning
+                self.bias[i,:] = self.bias_function(i,d_tanh)
+                self.weight_martix[i] -= self.bias[i] * self.learning
                 self.threshold_martix[i,:] -= self.bias[i,:] * self.learning
+
+            # print(self.weight_martix[0,0]) # For test usage
 
 
     # Calculate bias except the last level
     def bias_function(self,i,function):
-        return np.dot(function(self.Output_value[i,:]),self.loss_function(i))
+        return np.dot(function(self.Output_value[i]),self.loss_function(i))
 
     # Calculate loss except the last level
     def loss_function(self,i):
-        return np.dot(self.bias[i-1,:],self.weight_martix[i,:])
+        return np.dot(self.bias[i-1],self.weight_martix[i])
 
 
 class Output_layer:
@@ -119,7 +137,7 @@ class Output_layer:
     # Function for outputting calculation
     def output_result(self,input):
         # print(np.sum(np.dot(input,self.weight.T))) # For test usage
-        self.output = self.output_function(np.sum(np.dot(input,self.weight.T)),sigmoid)
+        self.output = self.output_function(np.sum(np.dot(input,self.weight.T)),tanh)
         return self.output
 
     # Tool Function - Not use directly
@@ -127,7 +145,7 @@ class Output_layer:
         return function(input-(self.threshold))
 
     def backward_learning(self,y_train):
-        self.bias = self.bias_function(y_train,d_sigmoid)
+        self.bias = self.bias_function(y_train,d_tanh)
         self.weight -= self.learning* self.bias
         self.threshold -= self.learning* self.bias
 
@@ -159,7 +177,7 @@ class NeuralNetwork:
 
     def train(self):
         print("Training active")
-        for i in tqdm(range(len(self.x_train)),desc="Training process"):
+        for i in tqdm(range(len(self.x_train)-59500),desc="Training process"):
             temp_output=self.output_layer.output_result(self.hidden_layer.forward_calculate(self.input_layer.active(i)))
             self.hidden_layer.backward_train(self.x_train[i],self.y_train[i],self.output_layer)
 
@@ -171,9 +189,10 @@ class NeuralNetwork:
         self.correct=0
         dynamic_correct_rate =0
         dynamic_correct_point = np.zeros(len(self.x_test),dtype=float)
-        for i in tqdm(range(len(self.x_test)),desc="Testing process"):
+        for i in tqdm(range(len(self.x_test)-9500),desc="Testing process"):
             predict_result = self.output_layer.output_result(self.hidden_layer.forward_calculate(self.input_layer.active(i)))
-            if predict_result == self.y_test[i]:
+            print(predict_result,self.y_test[i])
+            if judge_result(predict_result,self.y_test[i]):
                 self.correct+=1
                 dynamic_correct_rate = self.correct/(i+1)
                 dynamic_correct_point[i]=(dynamic_correct_rate)
@@ -197,7 +216,6 @@ if __name__ == '__main__':
 
     # Standardize
     x_train, x_test = x_train / 255.0, x_test / 255.0
-    y_train_std, y_test_std = y_train / 255.0, y_test / 255.0
 
     # The shape of the array
     print(x_train.shape, y_train.shape)  # 训练集的形状
@@ -211,19 +229,19 @@ if __name__ == '__main__':
     print("扁平化后的训练集图像形状:", x_train_flat.shape)
     print("扁平化后的测试集图像形状:", x_test_flat.shape)
 
-    model = NeuralNetwork(0.1,3,x_train_flat,y_train_std,x_test_flat,y_test)
-    print(model.hidden_layer.num_col)
-    # model.train()
+
+    model = NeuralNetwork(0.4,3,x_train_flat,y_train/10,x_test_flat,y_test/10)
+    model.train()
     model.predict()
 
     # For test usage
     # Learning rate
     #     Learning = 0.5
-    # a=Hidden_layer(784,2,Learning)
-    # b=Output_layer(784,Learning)
+    # a=Hidden_layer(784,2,0.5)
+    # b=Output_layer(784,0.5)
     # print(b.output_result(a.forward_calculate(x_train_flat[100,:])))
     # print(y_train[1])
     # print(a.weight_martix)
-    # for i,j in zip(x_train_flat,y_train_std):
+    # for i,j in zip(x_train_flat,y_train/10):
     #     a.backward_train(i,j,b)
     # print(a.weight_martix)
